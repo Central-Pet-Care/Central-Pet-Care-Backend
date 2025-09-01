8be9ed62f6797d7800049a81658f05727f1e23f5
U L R Kavindya <it23600898@my.sliit.lk>
2025-09-01 19:29:05 +0530

    User authuntication part is done 😌👍

---
+++ controllers/userController.js
@@ -0,0 +1,167 @@
+import User from "../models/user.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv";
+dotenv.config()
+
+export function getUser(req, res){
+
+    User.find().then(
+        (userList)=>{
+            res.json({
+                List: userList
+            })
+        }
+    ).catch(()=>{
+        res.json({
+            message: "Unsuccessfull."
+        })
+    })
+}
+
+export function createUser(req, res){
+
+    const newUserData = req.body
+
+    if(newUserData.type == "admin"){
+
+        if(req.user == null){
+            res.json({
+                message: "Please login as administrator to create admin accounts"
+            })
+            return
+
+        }
+
+         if(req.user.type != "admin"){
+            res.json({
+                message: "Please login as administrator to create admin accounts"
+            })
+            return
+
+        }
+
+        
+
+    }
+
+    newUserData.password = bcrypt.hashSync(newUserData.password, 10)
+    
+    const user = new User(newUserData)
+
+    user.save().then(()=>{
+        res.json({
+            message: "User created."
+        })
+    }).catch(()=>{
+        res.json({
+            message: "User not created."
+        })
+    })
+ }
+
+// export function createUser(req, res) {
+//     const newUserData = req.body;
+
+//     // Hash the password
+//     newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+
+//     // Create and save the new user
+//     const user = new User(newUserData);
+
+//     user.save()
+//         .then(() => {
+//             res.json({
+//                 message: "User created successfully."
+//             });
+//         })
+//         .catch(() => {
+//             res.json({
+//                 message: "User creation failed."
+//             });
+//         });
+// }
+
+ export function deleteUser(req, res){
+    User.deleteOne({name: req.params.email}).then(()=>{
+        res.json({
+            message: "User delete successfull."
+        })
+        
+    }).catch(()=>{
+        res.json({
+            message: "User delete unsuccessfully."
+        })
+    })
+}
+
+export function loginUser(req, res){
+
+    User.find({email: req.body.email}).then(
+        (users)=>{
+           if(users.length==0){
+            res.json({
+                message: "User not found."
+            })
+           }else{
+              const user = users[0]
+
+              const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password)
+
+              if(isPasswordCorrect){
+                const token = jwt.sign({ //generate the token
+                    email : user.email,
+                    firstName : user.firstName,
+                    lastName : user.lastName,
+                    isBlocked : user.isBlocked,
+                    type : user.type,
+                    profilePicture : user.profilePicture
+                }, process.env.SECRET)
+                
+                res.json({
+                    message : "User logged in.",
+                    token: token,
+                    user: {
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    type: user.type,
+                    profilePicture: user.profilePicture,
+                    email: user.email
+                    }
+                    
+                })
+
+              }else{
+                res.json({
+                    messsage: "Password incorrect."
+                })
+              }
+           }
+        })
+}
+
+export function isAdmin(req){
+     if(req.user == null){
+            return false 
+        }
+
+         if(req.user.type != "admin"){
+            return false
+        }
+
+        return true
+}
+
+export function isCustomer(req){
+     if(req.user == null){
+            return false 
+        }
+
+         if(req.user.type != "customer"){
+            return false
+        }
+
+        return true
+}
+
+
---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRouter from "./routes/userRouter.js";
+dotenv.config()
+
+const app = express();
+
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.use(
+    (req, res, next)=>{
+        
+    const token = (req.header("Authorization"))?.replace("Bearer ", "")
+    // console.log(token)
+
+    if(token != null){
+        jwt.verify(token, process.env.SECRET,(error, decoded)=>{
+            if(!error){
+                req.user = decoded
+                // console.log(decoded)
+            }
+        })
+    }
+    next()
+
+    }
+)
+
+const connectionString = process.env.MONGO_DB_URL;
+
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log("✅ Database connected.");
+  })
+  .catch((err) => {
+    console.log(" Database connection failed.");
+    console.log("Error details:", err.message);
+  });
+
+  app.use("/api/users", userRouter)
+
+
+app.listen(5000, () => {
+  console.log(" Server is started on port 5000");
+});
+
+  //"email": "admin@example.com" - admin
+  //"password": "AdminPass123"
+
+   // "email": "alice.williams@example.com" - customer
+   //"password": "CustomerPass123"
+
+   //"email": "jane.smith@example.com", -customer
+   //"password": "1234",
